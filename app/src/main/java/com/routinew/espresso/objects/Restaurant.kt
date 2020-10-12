63284a6d61b6167e3afcaf36ce8903f729d1afd0
Ryan Bacon <tservo@tservo.org>
2020-10-11 19:27:51 -0700

    Resources, ready for github

---
+++ app/src/main/java/com/routinew/espresso/objects/Restaurant.kt
@@ -0,0 +1,18 @@
+package com.routinew.espresso.objects
+
+
+class Restaurant(var name: String) {
+    var date_established: String? = null
+    var email: String? = null
+    var id: Int? = null
+    var street: String? = null
+    var suite: String? = null
+    var city: String? = null
+    var state: String? = null
+    var zip_code: Int? = null
+    var website: String? = null
+    var phone: String? = null
+
+
+
+}
---
+++ app/src/main/java/com/routinew/espresso/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.routinew.espresso.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
