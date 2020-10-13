dad574a3e76eea0ba25e9d5a00c533e19c698513
Ryan Bacon <tservo@tservo.org>
2020-10-12 23:12:07 -0700

    recyclerlist exists, now to wire to feed

--- app/src/main/java/com/routinew/espresso/objects/Restaurant.kt
+++ app/src/main/java/com/routinew/espresso/objects/Restaurant.kt
@@ -16,3 +16,5 @@ class Restaurant(var name: String) {
 
 
 }
+
+
--- app/src/main/java/com/routinew/espresso/ui/main/MainFragment.kt
+++ app/src/main/java/com/routinew/espresso/ui/main/MainFragment.kt
@@ -1,12 +1,17 @@
 package com.routinew.espresso.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.routinew.espresso.R
+import com.routinew.espresso.databinding.MainFragmentBinding
+import com.routinew.espresso.databinding.RestaurantBinding
+import com.routinew.espresso.objects.Restaurant
 
 class MainFragment : Fragment() {
 
@@ -16,14 +21,43 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+
+
+    private var _binding: MainFragmentBinding? = null
+    // only valid between onCreateView and onDestroyView
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.restaurantList.apply {
+            setHasFixedSize(true)
+
+            layoutManager = LinearLayoutManager(view.context)
+
+            adapter = RestaurantListAdapter(arrayOf(
+                Restaurant("joe"),
+                Restaurant("brad")
+            ))
+        }
+
+        binding.message.visibility = View.GONE
+        binding.restaurantList.visibility = View.VISIBLE
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
