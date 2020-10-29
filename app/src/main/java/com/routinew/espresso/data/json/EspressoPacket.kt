f052bc5068db6ab23fe2a2e8176bf02460673ba0
Ryan Bacon <tservo@tservo.org>
2020-10-29 00:20:08 -0700

    upgrade unit tests and prepare to work on json adapters

--- app/src/main/java/com/routinew/espresso/data/json/EspressoPacket.kt
+++ app/src/main/java/com/routinew/espresso/data/json/EspressoPacket.kt
@@ -1,7 +1,9 @@
 package com.routinew.espresso.data.json
 
 import com.routinew.espresso.objects.Restaurant
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 class EspressoPacket (
     val success: Boolean,
     val restaurants: List<Restaurant>
